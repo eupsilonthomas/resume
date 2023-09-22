d1203b8df7fe768fb62a043c52ca75c871d326bd
Evelyn Thomas <eupsilonthomas@gmail.com>
2023-09-22 11:29:42 -0700

    style adjust

--- app/(user)/words/layout.tsx
+++ app/(user)/words/layout.tsx
@@ -7,11 +7,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <main className="flex min-h-[79vh] center-container">
-      <div className="grid-cols m-auto">
-        <div className="center m-8">
+    <main className="flex flex-wrap min-h-[79vh] overflow-hidden justify-between space-x-2 ">
+      <div className="m-8">
+        <WordList/>
+      </div>
+      <div className="">
+        <div className="m-8">
           <div>{children}</div>
-          <WordList/>
         </div>
       </div>
     </main>
